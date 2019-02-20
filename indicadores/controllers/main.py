25cddf6f36b09f681163245a4434e6c50404de81
Hernan Neuman <hernanneuman15@gmail.com>
2019-02-20 19:05:52 -0400

    agregado cambios a la vista arreglado estructura del modulo indicadores

--- indicadores/controllers/main.py
+++ indicadores/controllers/main.py
@@ -10,7 +10,8 @@ import base64
 
 class MainController(http.Controller):
 
-    _logger = logging.getLogger(__name__)
+	_logger = logging.getLogger(__name__)
 
 	@http.route(['/indicadores'], auth='public', website=True)
-	return http.request.render('indicadores.inicio_indicador')
+	def inicio(self):
+		return http.request.render('indicadores.inicio_indicador')
